7f4b2adc14e97a76578b1683d31e8e6fc336d065
quocviet1998 <chunhoxsieuquay@gmail.com>
2020-07-26 19:53:30 +0700

    test

---
+++ tests/test_blank.py
@@ -0,0 +1,6 @@
+import blank
+
+def test_blank_package():
+    my_str = "minhng.info"
+    rev_str = blank.reverse(my_str)
+    assert rev_str == "ofni.gnhnim"
---
+++ tests/test_say_hello.py
@@ -0,0 +1,8 @@
+import blank
+
+def test_say_hello():
+    try:
+        blank.main.say_hello()
+        assert True
+    except:
+        assert False
